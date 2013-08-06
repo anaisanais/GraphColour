a47423d967b136be975b719880bb335252cd1107
nzozulya <tashka.z@gmail.com>
2013-08-06 23:34:08 +0300

    test

--- src/Solver.java
+++ src/Solver.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Solver {
 
     /**
-     * The main class
+     * The main clasdfgss
      */
     public static void main(String[] args) {
         try {
---
+++ src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ната
+ * Date: 06.08.13
+ * Time: 23:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
