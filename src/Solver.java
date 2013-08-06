b0fc42808c27150b90842b669a79f484579c2422
nzozulya <tashka.z@gmail.com>
2013-08-06 23:31:18 +0300

    test

---
+++ src/Solver.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+/**
+ * The class <code>Solver</code> is an implementation of a greedy algorithm to solve the knapsack problem.
+ *
+ */
+public class Solver {
+
+    /**
+     * The main class
+     */
+    public static void main(String[] args) {
+        try {
+
+            solve(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Read the iuff, solve it, and print the solution in the standard output
+     */
+    public static void solve(String[] args) throws IOException {
+
+
+    }
+
+}
